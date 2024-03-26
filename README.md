ed87755b83e69279020d99281e902d0e40df656d
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-03-27 00:36:54 +0100

    Add CI.

---
+++ README.md
@@ -0,0 +1,9 @@
+# EdaxCluster
+
+## Description
+This is a Python library to create an [Edax](https://github.com/abulmo/edax-reversi) cluster.
+It requires at least Python 3.10, and the libraries listed in pyproject.toml.
+
+## Installation
+Run `pip install .` to install the package and all its dependencies.
+
--- src/edax_cluster/worker.py
+++ src/edax_cluster/worker.py
@@ -10,7 +10,7 @@ class EdaxWorker(Worker):
 
     def execute_task(self, task: bytes) -> bytes:
         edax_task = EdaxTask.from_bytes(task)
-        result = Edax(self.exe_path, level=edax_task.level).solve_native(edax_task.pos)
+        result = Edax(self.exe_path, tasks=1, level=edax_task.level).solve_native(edax_task.pos)
         return str(result).encode()
 
     def on_cancel(self) -> None:
