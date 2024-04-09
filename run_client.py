0fb5b2bd0790d9a71fa39b51844a4e1949595832
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-04-10 01:51:37 +0200

    Add IP argument to client

--- run_client.py
+++ run_client.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from reversi import ScoredGame, read_file, write_file, scored_positions, empty_count_range_filtered, positions
 from rte import RemoteServer
 from edax_cluster import EdaxBatchClient, EdaxTask
@@ -6,13 +7,15 @@ from edax_cluster import EdaxBatchClient, EdaxTask
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
+    ip = sys.argv[1] if len(sys.argv) > 1 else "localhost"
+
     FILE = r"C:\Users\Dominic\source\repos\python-reversi\data\random_selfplay_from_e54.gs"
     scored_games: list[ScoredGame] = read_file(FILE)
-    scored_pos = empty_count_range_filtered(scored_positions(scored_games), 29, 30)
+    scored_pos = empty_count_range_filtered(scored_positions(scored_games), 0, 30)
     unsolved_pos = [sp.pos for sp in scored_pos if not sp.is_score_defined()]
     print(len(unsolved_pos))
 
-    server = RemoteServer("localhost:50051")
+    server = RemoteServer(f"{ip}:50051")
     client = EdaxBatchClient(server, refresh_time=10)
     tasks = [EdaxTask(pos, level=60) for pos in unsolved_pos]
     results = client.solve(tasks)
